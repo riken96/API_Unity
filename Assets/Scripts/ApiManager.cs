13df1a082d48e4825b9a0ad6896d7b82f4192eff
Riken Sonani <rikensonani96@gmail.com>
2023-12-25 10:50:58 +0530

    form data and authorization demo added

--- Assets/Scripts/ApiManager.cs
+++ Assets/Scripts/ApiManager.cs
@@ -7,6 +7,9 @@ public class ApiManager : MonoBehaviour
 {
     // The base URL of the API
     public string ApiUrl = "https://goldygamestudio.com/apiDemo/apiDemo.php";
+    public string ApiUrlAuth = "https://goldygamestudio.com/apiDemo/apiDemoAuth.php";
+    public string ApiUrlFormData = "https://goldygamestudio.com/apiDemo/apiDemoFormData.php";
+    private const string authToken = "your_secret_token";
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +22,18 @@ public class ApiManager : MonoBehaviour
 
         // Initiates a PUT request with a JSON body when the script starts
         StartCoroutine(PutRequest(ApiUrl, "{ \"key\": \"value\" }"));
+
+        // Initiates another POST request with data from a custom class
+        MyData myData = new MyData { key = "customKey", value = "customValue" };
+        StartCoroutine(PostRequestWithCustomClass(ApiUrl, myData));
+
+        // Initiates another GET request with Authorization
+        StartCoroutine(GetRequestWithAuthorization(ApiUrlAuth, authToken));
+
+        // Initiates a POST request with a Form Data
+        StartCoroutine(PostRequestFormData(ApiUrlFormData));
     }
+
 
     // Coroutine for handling a GET request
     IEnumerator GetRequest(string url)
@@ -100,4 +114,99 @@ public class ApiManager : MonoBehaviour
             }
         }
     }
+
+    // Coroutine for handling a POST request with data from a custom class
+    IEnumerator PostRequestWithCustomClass(string url, MyData data)
+    {
+        // Convert the custom class to JSON
+        string jsonBody = JsonUtility.ToJson(data);
+
+        // Using UnityWebRequest for a POST request
+        using (UnityWebRequest webRequest = new UnityWebRequest(url, "POST"))
+        {
+            // Setting up the request with a JSON body
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonBody);
+            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+
+            // Sending the request and waiting for a response
+            yield return webRequest.SendWebRequest();
+
+            // Checking if the request was successful or resulted in an error
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(webRequest.error);
+            }
+            else
+            {
+                // Logging the response text if successful
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+
+    // Coroutine for handling a GET request with Authorization
+    IEnumerator GetRequestWithAuthorization(string url, string authToken)
+    {
+        // Using UnityWebRequest for a GET request
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            // Set the authorization header
+            webRequest.SetRequestHeader("Authorization", authToken);
+
+            // Sending the request and waiting for a response
+            yield return webRequest.SendWebRequest();
+
+            // Checking if the request was successful or resulted in an error
+            if (webRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(webRequest.error);
+            }
+            else
+            {
+                // Logging the response text if successful
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+    // Coroutine for handling a POST request with Form Data
+    IEnumerator PostRequestFormData(string url)
+    {
+        // Step 1: Create a new instance of WWWForm to prepare form data
+        WWWForm form = new WWWForm();
+
+        // Step 2: Add form fields (key-value pairs) to the form
+        form.AddField("username", "JohnDoe");
+        form.AddField("score", 100);
+
+        // Step 3: Create a UnityWebRequest for a POST request to the specified API URL
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+
+        // Step 4: Send the UnityWebRequest and wait for the request to complete
+        yield return www.SendWebRequest();
+
+        // Step 5: Check if the request was successful or resulted in an error
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            // Step 6: Log success messages and server response if successful
+            Debug.Log("Form upload complete!");
+            Debug.Log("Server response: " + www.downloadHandler.text);
+        }
+        else
+        {
+            // Step 7: Log an error message if the request encountered an error
+            Debug.LogError("Form upload error: " + www.error);
+        }
+    }
+}
+
+[System.Serializable]
+public class MyData
+{
+    public string key;
+    public string value;
 }
